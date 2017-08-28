725a24272a412e74a42a69fbc5b0833fa5f3fa96
Terry Lewis <terry1994dev@gmail.com>
2017-08-28 12:12:54 +1000

    Renamed Icon directory to Resources. Added a bunch of missing (possibly random) files from RestKit dependencies. Changed icon to blue style. Top shelf image now uses filled Ping icon with background and layout similar to that of the Settings application. Renamed Queue to Download Queue. Remove logging from settings manager upon configuring Sonarr API.

---
+++ ISO8601ForCocoa/build/ISO8601ForCocoa.build/Debug-appletvos/ISO8601.build/DerivedSources/ISO8601_vers.c
@@ -0,0 +1,5 @@
+extern const unsigned char ISO8601VersionString[];
+extern const double ISO8601VersionNumber;
+
+const unsigned char ISO8601VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ISO8601  PROJECT:ISO8601ForCocoa-1" "\n";
+const double ISO8601VersionNumber __attribute__ ((used)) = (double) 1.;
